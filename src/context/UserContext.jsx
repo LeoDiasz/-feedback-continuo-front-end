0daa0d5c8b167005608d948df0017c7b65b2766b
Leonardo Diasz <leonardoduarte.multimidia@gmail.com>
2022-08-19 17:10:52 -0300

    resolvendo problemas na home

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -9,12 +9,14 @@ const UserProvider = ({ children }) => {
   const [user, setUser] = useState()
   const [collaborator, setCollaborator] = useState()
   const { signIn } = useAuthContext()
-
   const [listCollaborators, setListCollaborators] = useState()
+  const [usersPerPage, setUsersPerPage] = useState(10)
+  const [atualPage, setAtualPage] = useState(0)
+  const [pages, setPages] = useState(0)
 
   const getDatasCollaboratorById = async (id) => {
     try {
-      const { data: datasCollaborator } = await apiDbc.get(`/users/recover-user?id=${id}`)
+      const { data: datasCollaborator } = await apiDbc.get(`/users/recover-users?id=${id}`)
 
       datasCollaborator.avatar = datasCollaborator.avatar ? "data:image/png;base64," + datasCollaborator.avatar : null
 
@@ -29,7 +31,6 @@ const UserProvider = ({ children }) => {
   const getDatasUser = async () => {
     try {
       const { data: datasUser } = await apiDbc.get("/users/recover-logged-user")
-
       datasUser.avatar = datasUser.avatar ? "data:image/png;base64," + datasUser.avatar : null
 
       setUser(datasUser)
@@ -40,12 +41,6 @@ const UserProvider = ({ children }) => {
       return { id: null }
     }
   }
-
-  //////////////////////////////////////////////////
-
-  const [usersPerPage, setUsersPerPage] = useState(10)
-  const [atualPage, setAtualPage] = useState(0)
-  const [pages, setPages] = useState(0)
 
   const getListCollaborators = async () => {
 
@@ -84,16 +79,17 @@ const UserProvider = ({ children }) => {
 
       const { data: userDatasReturn } = await apiDbc.post("/users/create", userFormated)
 
-      formData.append("id", userDatasReturn.idUser)
-
       const userLogin = {
         login: userDatas.email,
         senha: userDatas.password
       }
 
+      await signIn(userLogin)
+
+      formData.append("id", userDatasReturn.idUser)
+      
       await apiDbc.put(`/users/update-file`, formData, { headers: { 'Content-Type': 'multipart/form-data' } })
 
-      await signIn(userLogin)
     
     } catch (Error) {
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -14,7 +14,6 @@ export const Home = () => {
   const [loading, setLoading] = useState(true)  
   
   const setup = async () => {
-
     const {id} = await getDatasUser()
     await getFeedbacksUser("receveid", id)
     await getFeedbacksUser("gived", id)
