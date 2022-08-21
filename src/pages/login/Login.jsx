e44ef37f06b751a67bcb8e63723eb020fd7b5273
Lucas Araujo <lucas.gouvea@dbccompany.com.br>
2022-08-21 20:24:52 -0300

    opção de visibilidade da senha no login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,35 +1,38 @@
 import { Formik } from "formik"
+import { useState } from 'react'
+import { MdOutlineVisibility, MdOutlineVisibilityOff } from 'react-icons/md'
 import { useAuthContext } from "../../hooks/useAuthContext"
 import { ScreenAndRegisterUser } from '../../components/ScreenLoginAndRegisterUser'
 import { LoginSchema } from "../../utils/validationsSchema"
 import { Button } from '../../components/Button/styles'
 import { DivTextValidation, InputField, TextValidation, Label } from "../../components/InputStyles/styles"
-import { FormDiv} from "./styles";
+import { FormDiv } from "./styles";
 import { Loading } from "../../components/Loading"
 
 export const Login = () => {
-  const {signIn, isLoading} = useAuthContext()
+  const { signIn, isLoading } = useAuthContext()
+  const [passwordShow, setPasswordShow] = useState(false)
 
-  if(isLoading) {
+  if (isLoading) {
     return (
-      <Loading/>
+      <Loading />
     )
   }
 
   return (
     <ScreenAndRegisterUser titleForm="Entrar na conta" isScreenLogin>
       <Formik
-        initialValues={{login: "", senha: ""}}
+        initialValues={{ login: "", senha: "" }}
         validationSchema={LoginSchema}
-        onSubmit = {values => {
+        onSubmit={values => {
           signIn(values)
         }}
       >
-        {({errors}) =>(
+        {({ errors }) => (
           <FormDiv>
             <div>
               <Label htmlFor="login">E-MAIL</Label>
-              <InputField name="login" id="login" placeholder="Digite seu e-mail"/>
+              <InputField name="login" id="login" placeholder="Digite seu e-mail" />
               <DivTextValidation>
                 <TextValidation>{errors.login}</TextValidation>
               </DivTextValidation>
@@ -37,14 +40,15 @@ export const Login = () => {
 
             <div>
               <Label htmlFor="senha">SENHA</Label>
-              <InputField name="senha" id="senha" placeholder="Digite sua senha"/>
+              <InputField name="senha" id="senha" type={passwordShow ? 'text' : 'password'} placeholder="Digite sua senha" />
+              <button onClick={() => setPasswordShow(passwordShow ? false : true)}>{passwordShow ? <MdOutlineVisibility /> : <MdOutlineVisibilityOff />}</button>
               <DivTextValidation>
                 <TextValidation>{errors.senha}</TextValidation>
               </DivTextValidation>
             </div>
             <Button type="submit" id="id-button-login" disabled={Object.values(errors).length > 0}>ENTRAR</Button>
           </FormDiv>
-         )} 
+        )}
       </Formik>
     </ScreenAndRegisterUser>
   )
