68d75a57a11a72ca4834043f15ba8a3606def4e9
Lucas Araujo <lucas.gouvea@dbccompany.com.br>
2022-08-22 09:53:39 -0300

    testando paginação com botões no feedback

--- src/components/FeedbackList/ComponentsPages/Pager.jsx
+++ src/components/FeedbackList/ComponentsPages/Pager.jsx
@@ -1,8 +1,12 @@
-export const Pager = ({pages}) => {
+export const Pager = ({pages, setCurrentPage}) => {
   return (  
     <div>
       {Array.from(Array(pages), (item, index) => {
-        return <button key={index}>{index+1}</button>
+        return <button 
+        key={index}
+        value={index}
+        onClick={(e)=> setCurrentPage(e.target.value) }
+        >{index+1}</button>
       })}
     </div>
   )
